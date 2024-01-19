d2331c53c6b3d13e41fdd0a4599599a10c7b52a2
Lucas Madeira <lmadeira.ec@gmail.com>
2024-01-19 14:30:58 -0400

    configurando arquivos de configuração e profiles da aplicação

---
+++ infrastructure/src/main/java/br/com/lucamadeira/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.com.lucamadeira.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.lucamadeira.admin.catalogo")
+public class WebServerConfig {
+}
